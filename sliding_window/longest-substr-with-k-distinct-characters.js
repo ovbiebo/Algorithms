203a7a1b48118532626be7335c35635cef68f55a
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-08-08 13:46:42 +0100

    Longest Substring with K Distinct Characters solved

---
+++ sliding_window/longest-substr-with-k-distinct-characters.js
@@ -0,0 +1,22 @@
+const longest_substring_with_k_distinct = (str, k) => {
+    let result = 0;
+    let counts = new Map();
+    let length = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        counts.set(str[i], (counts.get(str[i]) || 0) + 1)
+        length++;
+        while (counts.size > k){
+            const initial = str[i - --length]
+            initial && counts.set(initial, (counts.get(initial)) - 1);
+            !counts.get(initial) && counts.delete(initial)
+        }
+        result = result ? Math.max(length, result) : length;
+    }
+
+    return result;
+};
+
+console.log(longest_substring_with_k_distinct("araaci", 2)); // 4
+console.log(longest_substring_with_k_distinct("araaci", 1)); // 2
+console.log(longest_substring_with_k_distinct("cbbebi", 3)); // 5
