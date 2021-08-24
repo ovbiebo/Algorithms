78fb48385be70c4780730190880204a3619e4b6d
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-08-24 12:52:45 +0100

    Concatenation of all given words solved with O(N) runtime

--- sliding_window/concat-of-all-given-words.js
+++ sliding_window/concat-of-all-given-words.js
@@ -1,41 +1,93 @@
-const find_word_concatenation = (str, words) => {
+// const find_word_concatenation = (s, words) => {
+//     let result = [],
+//         pattern = {},
+//         matches = {},
+//         wordLength = words[0].length,
+//         front = wordLength * words.length,
+//         back = 0;
+//
+//     for (const word of words) {
+//         pattern[word] = (pattern[word] || 0) + 1;
+//     }
+//
+//     while (front <= s.length) {
+//         let i = front;
+//         let word;
+//
+//         while (i > back) {
+//             word = s.slice(i - wordLength, i)
+//
+//             if (pattern[word] && (matches[word] ?? 0) < pattern[word]) {
+//                 matches[word] = (matches[word] ?? 0) + 1;
+//                 i -= wordLength;
+//             } else {
+//                 matches = {}
+//                 break;
+//             }
+//         }
+//
+//         if (i === back) {
+//             matches = {};
+//             result.push(i)
+//         }
+//
+//         back++;
+//         front++;
+//     }
+//
+//     return result;
+// };
+
+const find_word_concatenation = (s, words) => {
     let result = [],
-        matches = {},
         pattern = {},
-        total = 0,
-        wordLength = words[0].length,
-        r = wordLength;
+        wordLength = words[0].length;
 
     for (const word of words) {
         pattern[word] = (pattern[word] || 0) + 1;
     }
 
-    while (r <= str.length) {
-        let word = str.slice(r - wordLength, r)
-        if (pattern[word]) {
-            matches[word] = (matches[word] || 0) + 1;
-            total++;
+    for (let i = 0; i < wordLength; i++) {
+        let back = i,
+            front = back + wordLength,
+            matches = {},
+            count = 0
 
-            while (matches[word] > pattern[word]) {
-                let k = str.slice(r - total * wordLength, r - --total * wordLength);
-                matches[k] -= 1;
+        while (front <= s.length) {
+            let word = s.slice(front - wordLength, front);
+
+            if (pattern[word]) {
+                matches[word] = (matches[word] ?? 0) + 1;
+                count++;
+
+                while (matches[word] > pattern[word]) {
+                    matches[s.slice(back, back + wordLength)] -= 1;
+                    back += wordLength;
+                    count--;
+                }
+
+                if (count === words.length) {
+                    result.push(back)
+                }
+            } else {
+                matches = {}
+                count = 0;
+                back = front;
             }
 
-            if (total === words.length) {
-                result.push(r - total * wordLength);
-            }
-            r += wordLength;
-        } else {
-            matches = {};
-            total = 0;
-            r++;
+            front += wordLength;
         }
     }
 
     return result;
 };
 
+console.log(find_word_concatenation("a", ["a"])); // [0]
+console.log(find_word_concatenation("foobarfoobarthefoobarman", ["foo", "bar"])); // [0,3,6,15]
 console.log(find_word_concatenation("catfoxcat", ["cat", "fox"])); // [0, 3]
 console.log(find_word_concatenation("catcatfoxfox", ["cat", "fox"])); // [3]
 console.log(find_word_concatenation("wordgoodgoodgoodbestword", ["word", "good", "best", "word"])); // []
+console.log(find_word_concatenation("barfoothefoobarman", ["foo", "bar"])); // [0, 9]
 console.log(find_word_concatenation("barfoofoobarthefoobarman", ["bar", "foo", "the"])); // [6,9,12]
+console.log(find_word_concatenation("aaaaaaaaaaaaaa", ["aa", "aa"])); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(find_word_concatenation("lingmindraboofooowingdingbarrwingmonkeypoundcake", ["fooo", "barr", "wing", "ding", "wing"])); // [13]
