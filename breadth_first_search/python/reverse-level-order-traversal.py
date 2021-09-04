242840c1e324c79b5165dba60dbb63d9f06f30c0
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-09-04 07:32:23 +0100

    Reverse Level Order Traversal solved in python

---
+++ breadth_first_search/python/reverse-level-order-traversal.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+    def __repr__(self):
+        return str(self.val)
+
+
+def traverse(root):
+    result = deque()
+    if root is None:
+        return result
+
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+        level = []
+        level_size = len(queue)
+
+        for i in range(level_size):
+            current_node: TreeNode = queue.popleft()
+            level.append(current_node)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+
+        result.appendleft(level)
+
+    return list(result)
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level order traversal: " + str(traverse(root)))
+
+
+main()
