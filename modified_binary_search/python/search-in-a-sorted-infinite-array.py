8389f5342148c5221d19ea723df50635b04026cb
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-11-11 12:36:33 +0100

    Search in a Sorted Infinite Array solved in python

---
+++ modified_binary_search/python/search-in-a-sorted-infinite-array.py
@@ -0,0 +1,44 @@
+import math
+
+
+class ArrayReader:
+    def __init__(self, arr):
+        self.arr = arr
+
+    def get(self, index):
+        if index >= len(self.arr):
+            return math.inf
+        return self.arr[index]
+
+
+def search_in_infinite_array(reader, key):
+    start, end = 0, 1
+
+    while reader.get(end) < key:
+        new_start = end + 1
+        end += (end - start + 1) * 2
+        start = new_start
+
+    return binary_search(reader, start, end, key)
+
+
+def binary_search(reader, start, end, key):
+    while start <= end:
+        mid = start + (end - start) // 2
+        if reader.get(mid) == key:
+            return mid
+        if reader.get(mid) < key:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return -1
+
+
+if __name__ == '__main__':
+    reader = ArrayReader([4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30])
+    print(search_in_infinite_array(reader, 16))
+    print(search_in_infinite_array(reader, 11))
+    reader = ArrayReader([1, 3, 8, 10, 15])
+    print(search_in_infinite_array(reader, 15))
+    print(search_in_infinite_array(reader, 200))
